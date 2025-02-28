cbb0a07cb6f1828737a0e407148f0385cd661e49
mehedi <mohiuddinmahadi08@gmail.com>
2025-02-28 12:11:29 +0600

    init

---
+++ app/src/test/java/com/bongoacademy/webiew_pro/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package com.bongoacademy.webiew_pro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        // This test verifies that 2 + 2 equals 4
+        assertEquals(4, 2 + 2);
+    }
+}
